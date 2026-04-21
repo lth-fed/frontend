95005d8b5bb9c0ba73a330b34d6b534a249ef89d
Nelli Skogman <nelli.skogman@gmail.com>
2026-04-21 20:51:47 +0200

    [INFRA] (Refactor): Refactor hooks

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
 
-if [[ ! -d ${1:-"../hooks"} ]]; then
+if [ ! -d "${1:-"../hooks"}" ]; then
     echo "You need to have the hooks repo downloaded at '../hooks' or specify it's location as a parameter."
     exit 1
 fi
 
-bash "${1:-"$PWD/../hooks"}/install-hooks.sh" "${1:-"$PWD/../hooks"}"
+"${1:-"$PWD/../hooks"}/install-hooks.sh" "${1:-"$PWD/../hooks"}"
