eccd0f64a826b9166d2802344da284de8c475681
Icelk <main@icelk.dev>
2026-05-21 22:13:03 +0200

    [APP] (Update): validate stil-id

--- auth/src/routes/providers/test/+page.svelte
+++ auth/src/routes/providers/test/+page.svelte
@@ -9,7 +9,13 @@
 
 	const query = new URLSearchParams(location.search)
 
+	const sid = /[a-z][a-z][0-9]{4}[a-z][a-z]-s/
+
 	async function click() {
+		if (!sid.test(stil_id)) {
+			alert('Välj ett korrekt stil-id. Tack!')
+			return
+		}
 		const body = {
 			id: query.get('id'),
 			stil_id,
