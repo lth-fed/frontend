a15114d503bedfb7c8b92cc6fa82eea2f87a1271
Icelk <main@icelk.dev>
2026-05-25 13:26:34 +0200

    [APP] (Update): Initial back button handling

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,4 +1,13 @@
 import { getTextDirection, getLocale } from '$lib/paraglide/runtime';
+import { App as CapacitorApp } from '@capacitor/app';
+
+CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+	if (!canGoBack) {
+		CapacitorApp.exitApp();
+	} else {
+		window.history.back();
+	}
+});
 
 export const init = () => {
 	const locale = getLocale();
