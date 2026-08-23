0dd159e7ccf67afe6ce3ef474d303336c4101916
Icelk <main@icelk.dev>
2026-08-23 13:28:02 +0200

    [INFRA] (Update): Improve build script

--- build-push.sh
+++ build-push.sh
@@ -1,6 +1,14 @@
 #!/bin/env bash
 
+if [ $(git status --porcelain | wc -l) -ne "0" ]; then
+    echo Please commit you changes before building.
+    exit 1
+fi
+
 read -p "Version: " version
+
+git tag $version
+git push --tags
 
 pnpm install --frozen-lockfile
 pnpm build
